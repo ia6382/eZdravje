924d0527a84363b42dad4184301b5c7cc580b135
Ivan Antešić <ia6382@student.uni-lj.si>
2016-06-04 20:34:24 +0000

    funkcionalno narejeno

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -30,6 +30,8 @@ var aliTretji = 0;
 var meritve1 = [[130,133,132,140,130,130,135,128],[110,112,108,115,120,121,116,114],[140,144,145,143,150,148,151,140]];
 var meritve2 = [[80,81,82,83,80,79,80,78],[60,62,64,60,67,70,66,64],[100,95,97,94,102,100,99,97]];
 
+var diagnoza = 2; // 1 je nizek,2 je dober, 3 je visok
+
 /**
  * Generator podatkov za novega pacienta, ki bo uporabljal aplikacijo. Pri
  * generiranju podatkov je potrebno najprej kreirati novega pacienta z
@@ -40,6 +42,50 @@ var meritve2 = [[80,81,82,83,80,79,80,78],[60,62,64,60,67,70,66,64],[100,95,97,9
  */
  
 var ehrId;
+
+function dodajMeritve() {
+	var sessionId = getSessionId();
+	
+    var ehrIdRocni = $("#EHR").val();
+
+	if (!ehrIdRocni || ehrIdRocni.trim().length == 0) {
+		$("#dodajMeritveSporocilo").html("<span class='obvestilo "+"label label-default'>Izpolnite zahtevana polja.</span>").fadeOut(10000);
+	} else {
+	    
+		$.ajaxSetup({
+		    headers: {"Ehr-Session": sessionId}
+		});
+		
+		var podatki = {
+		    "ctx/language": "en",
+		    "ctx/territory": "SI",
+		    "ctx/time": $("#DatumInUra").val(),
+		    "vital_signs/blood_pressure/any_event/systolic": $("#TlakSistolicni").val(),
+		    "vital_signs/blood_pressure/any_event/diastolic": $("#TlakDiastolicni").val()
+		};
+		
+		var queryParams = {
+        "ehrId": ehrIdRocni,
+        templateId: 'Vital Signs',
+        format: 'FLAT',
+        committer: 'Belinda Nurse'
+        };
+        
+		$.ajax({
+		    url: baseUrl + "/composition?" + $.param(queryParams),
+		    type: 'POST',
+		    contentType: 'application/json',
+		    data: JSON.stringify(podatki),
+		    success: function (res) {
+		        $("#dodajMeritveSporocilo").html("<span class='obvestilo "+"label label-default'>Uspešno dodane meritve.</span>").fadeOut(10000);
+		    },
+		    error: function(err) {
+		    	$("#dodajMeritveSporocilo").html("<span class='obvestilo "+"label label-default'>Prišlo je do napake: " + JSON.parse(err.responseText).userMessage + ".").fadeOut(10000);
+		    }
+		});
+	}
+}
+
 function generirajPodatke(stPacienta) {
     if(stPacienta == 1){
         aliPrvi = 1;
@@ -186,12 +232,17 @@ function izberiEhrId() {
 	}
 }
 
+
 function pokaziMeritev(i){
     $("#zgornji"+i).toggle();
     $("#spodnji"+i).toggle();
 }
 
 function preberiMeritveVitalnihZnakov(){
+    $("#panel1").show();
+    $("#panel2").show();
+    $("#panel3").show();
+    $("#panel4").hide();
     var sessionId = getSessionId();
     ehrId = $("#meritveVitalnihZnakovEHRid").val();
     $.ajaxSetup({
@@ -209,7 +260,7 @@ function preberiMeritveVitalnihZnakov(){
             var podatki = new Array(res.length);
 
             for (var i in res) {
-                podatki[i] = {"category": (res[stPodatkov-i].time).substring(11,16),
+                podatki[i] = {"category": (res[i].time).substring(5,10)+" ob "+(res[i].time).substring(11,16),
 							"column-1": res[stPodatkov-i].systolic,
 							"column-2": res[stPodatkov-i].diastolic
                             };
@@ -222,6 +273,13 @@ function preberiMeritveVitalnihZnakov(){
                             <tr>\
                                 <td class='text-right' id='spodnji"+i+"' style='display:none'>"+"spodnji (diastolični): "+res[i].diastolic+" "+res[i].unit+"</td>\
                             </tr>";
+                            
+                if(res[stPodatkov-i].systolic < 90 || res[stPodatkov-i].diastolic < 60){
+                    diagnoza = 1;
+                }
+                if(res[stPodatkov-i].diastolic > 140 || res[stPodatkov-i].diastolic > 90){
+                    diagnoza = 3;
+                }
             }
             results += "</table>";
 			$("#result").html(results);
@@ -253,31 +311,54 @@ function preberiMeritveVitalnihZnakov(){
 					"trendLines": [],
 					"graphs": [
 						{
-							"balloonText": "[[title]] ob [[category]], vrednost [[value]]",
+							"balloonText": "[[title]] dne [[category]], vrednost [[value]]",
 							"bullet": "round",
-							"lineThickness": "6",
+							"bulletSize": 12,
+							"lineThickness": 7,
 							"id": "AmGraph-1",
 							"title": "sistolični tlak",
-							"valueField": "column-1",
-							"fillAlphas": 0.7,
-							"lineAlpha": 0
+							"valueField": "column-1"
 						},
 						{
 							"balloonText": "[[title]] ob [[category]], vrednost [[value]]",
 							"bullet": "square",
-							"lineThickness": "6",
+							"bulletSize": 12,
+							"lineThickness": 7,
 							"id": "AmGraph-2",
 							"title": "diastolični tlak",
-							"valueField": "column-2",
-							"fillAlphas": 0.7,
-							"lineAlpha": 0
+							"valueField": "column-2"
 						}
 					],
-					"guides": [],
+					"guides": [{
+                        "value": 90,
+                        "toValue": 140,
+                        "lineColor": "#CC0000",
+                        "lineAlpha": 1,
+                        "fillAlpha": 0.2,
+                        "fillColor": "#CC0000",
+                        "dashLength": 2,
+                        "inside": true,
+                        "label": "normalen sistolični tlak",
+                        "valueAxis": "ValueAxis-1"
+                    },
+                    {
+                        "value": 60,
+                        "toValue": 90,
+                        "lineColor": "#FFFF00",
+                        "lineAlpha": 1,
+                        "fillAlpha": 0.2,
+                        "fillColor": "#FFFF00",
+                        "dashLength": 2,
+                        "inside": true,
+                        "label": "normalen diastolični tlak",
+                        "valueAxis": "ValueAxis-1"
+                    }],
 					"valueAxes": [
 						{
 							"id": "ValueAxis-1",
-							"title": "mm[Hg]"
+							"title": "mm Hg",
+							"maximum": 180,
+							"minimum": 60
 						}
 					],
 					"allLabels": [],
@@ -297,8 +378,79 @@ function preberiMeritveVitalnihZnakov(){
 				}
 			);
 		//-----konec grafa
-			
+			$("#razlaga").html("<p>"+diagnoza+"</p>");
         }
     });
-}
+    
+    }
+    
+    function pokaziZemljevid(ustanova){
+        $("#panel4").show();
+        var map;
+        var infoWindow;
+
+        //function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 15
+        });
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            
+          navigator.geolocation.getCurrentPosition(function(position) {
+              
+            var pos = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+            infoWindow = new google.maps.InfoWindow({map: map});
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Vaša približna lokacija.');
+            map.setCenter(pos);
+            
+            var service = new google.maps.places.PlacesService(map);
+            service.nearbySearch({
+                location: pos,
+                radius: 500000,
+                //name: ['ZD', 'zdravstveni dom', 'bolnica', 'UKC', 'bolnišnica', 'hospital', 'ambulanta'],
+                type: [ustanova]
+            }, callback);
+            
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+          
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      //}
+
+      function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
+        }
+      }
+
+      function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infoWindow.setContent(place.name);
+          infoWindow.open(map, this);
+        });
+      }
+      
+      
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
+    }
 
